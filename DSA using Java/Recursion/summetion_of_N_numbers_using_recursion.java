e6a738c16969b425dfdac9ba50bede5510d911de
vikramdesai123 <121470452+vikramdesai123@users.noreply.github.com>
2024-07-30 07:38:25 +0000

    Rucursive way to add numbers using parameters

---
+++ DSA using Java/Recursion/summetion_of_N_numbers_using_recursion.java
@@ -0,0 +1,18 @@
+public class summetion_of_N_numbers_using_recursion {
+    
+    public void summetion(int i, int sum){
+        
+        if(i<1){
+            System.out.println("total sum:"+ sum );
+            return;            
+        }
+        summetion(i-1, sum+i);
+    }
+    
+    public static void main(String[] args) {
+        int n = 5;
+
+        summetion_of_N_numbers_using_recursion s = new summetion_of_N_numbers_using_recursion();
+        s.summetion(n, 0);
+    }
+}
