c6283eb20bae376efffe5d7e07f29480ed8fb371
vikramdesai123 <121470452+vikramdesai123@users.noreply.github.com>
2024-07-30 06:33:13 +0000

    printed 1 to N numbers using backtracking

---
+++ DSA using Java/Recursion/print_1_to_N_using_backtracking.java
@@ -0,0 +1,20 @@
+
+    // using backtracking concepts
+
+
+public class print_1_to_N_using_backtracking {
+
+    public void printNum(int i, int n){
+        if(i<1){
+            return;
+        }
+        // i--;
+        printNum(i-1, n);
+        System.out.println(i);
+
+    }
+    public static void main(String[] args) {
+        print_1_to_N_using_backtracking p = new print_1_to_N_using_backtracking();
+        p.printNum(5, 5);
+    }
+}
